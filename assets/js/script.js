847534524aeb28ea87acab4fd3e4150487a8dd1f
AndersonJuhasc <anderson.juhasc@gmail.com>
2012-02-15 16:16:43 -0200

    Atualizacao baseada no h5bp

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,8 @@
-/* Author: 
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(function() {
+	// GET URL PARAMETER
+	function getURLParameter(name) {
+    return decodeURI(
+        (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+    );
+	}
+}(jQuery));
